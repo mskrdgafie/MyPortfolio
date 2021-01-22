98fb41bb4e0d07d40a53fd50030037f2ed39a1f3
Misiker <misikerd80@gmail.com>
2021-01-22 10:30:40 +0300

    projects model is added.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Project)
+admin.site.register(models.ProjectImage)
+admin.site.register(models.About)
+admin.site.register(models.Skill)
+admin.site.register(models.Contact)
+admin.site.register(models.SocialProfile)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=30, default='')
+    category = models.CharField(max_length=30)
+    client = models.CharField(max_length=100)
+    project_date = models.DateTimeField()
+    project_url = models.URLField()
+    project_description = models.TextField()
+    github_link = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='projects', on_delete=models.CASCADE)
+    project_image = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return self.project.title
+
+class About(models.Model):
+    full_name = models.CharField(max_length=100)
+    job_name = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=50)
+    full_description = models.TextField()
+    website = models.URLField()
+    phone = models.CharField(max_length=15)
+    my_address = models.CharField(max_length=50)
+    degree = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.full_name
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    percentage = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=15)
+    email = models.EmailField()
+    subject = models.CharField(max_length=30)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class SocialProfile(models.Model):
+    name = models.CharField(max_length=30)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
