6ae8ff2e65091591dab717f601458537d58bcd4e
Misiker Degafie <67056825+mskrdgafie@users.noreply.github.com>
2021-01-23 12:01:34 +0300

    Update settings.py

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#dzqap_%wac#4fz8x2w&dy8e1j18!13c#-*=@(h+mc@z5f0%cb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','http://mskr.pythonanywhere.com/']
 
 
 # Application definition
